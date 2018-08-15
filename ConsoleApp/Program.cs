4819188fec2be3ab4d36483019e0d68c2373ee81
Christopher Martel <cmarte@lsuhsc.edu>
2018-08-15 15:34:55 -0500

    Adding Hello World app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "HELLO WORLD";
+            int i = 0;
+
+            while (i < hello.Length)
+            {
+                Console.WriteLine(hello.Substring(0, i));
+                i++;
+            }
+            while (i >= 0)
+            {
+                Console.WriteLine(hello.Substring(0, i));
+                i--;
+            }
+
+            Console.WriteLine("Shall we have another go? Press 'Q' to quit, any other key to continue");
+            if (Console.ReadKey().Key != ConsoleKey.Q)
+                Main(null);
+        }
+    }
+}
