aa3d35ed8f18db62420031c23f4f048a9e20f903
Christopher Martel <cmarte@lsuhsc.edu>
2018-09-07 09:13:11 -0500

    Adding code to display teams sorted by Team properties. Adding NFL 2018 data.

---
+++ ConsoleApp/LeagueBuilder.cs
@@ -0,0 +1,147 @@
+﻿using FootballObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace ConsoleApp
+{
+    public class LeagueBuilder : IDisposable
+    {
+        public LeagueBuilder()
+        {
+        }
+
+        public League BuildNFLLeague(string statsFilePath, string leagueFilePath)
+        {
+            return BuildNFLLeague(statsFilePath, leagueFilePath, DateTime.Now);
+        }
+
+        public League BuildNFLLeague(string statsFilePath, string leagueFilePath, DateTime onDate)
+        {
+            XmlDocument data = new XmlDocument();
+            data.Load(statsFilePath);
+            List<Game> allGames = PreLoadGames(data, onDate);
+
+            data.Load(leagueFilePath);
+            var nflStructure = data["NFLLeague"]["Conferences"].ChildNodes.Cast<XmlNode>();
+
+            League league = new League(
+                "NFL",
+                nflStructure.Select(c =>
+                    new Conference(
+                        c["ConferenceName"].InnerText,
+                        c["Divisions"].Cast<XmlNode>().Select(d => new Division(
+                            d["DivisionName"].InnerText,
+                            d["Teams"].Cast<XmlNode>().Select(t => new Team(
+                                t["City"].InnerText,
+                                t["NickName"].InnerText,
+                                t["ID"].InnerText,
+                                allGames.Where(g => g.HomeTeam == t["ID"].InnerText || g.AwayTeam == t["ID"].InnerText).Select(g => new Game(
+                                    g.Date,
+                                    g.Week,
+                                    g.HomeTeam,
+                                    g.AwayTeam,
+                                    g.HomePoints,
+                                    g.AwayPoints,
+                                    new GameStats(
+                                        g.HomeStats.Touchdowns,
+                                        g.HomeStats.TouchdownsAllowed,
+                                        g.HomeStats.FieldGoals,
+                                        g.HomeStats.FirstDowns,
+                                        g.HomeStats.PassingYards,
+                                        g.HomeStats.RushingYards,
+                                        g.HomeStats.Penalties,
+                                        g.HomeStats.PenaltyYards,
+                                        g.HomeStats.Turnovers,
+                                        g.HomeStats.Punts,
+                                        g.HomeStats.PuntYards,
+                                        g.HomeStats.TimeOfPosession),
+                                    new GameStats(
+                                        g.AwayStats.Touchdowns,
+                                        g.AwayStats.TouchdownsAllowed,
+                                        g.AwayStats.FieldGoals,
+                                        g.AwayStats.FirstDowns,
+                                        g.AwayStats.PassingYards,
+                                        g.AwayStats.RushingYards,
+                                        g.AwayStats.Penalties,
+                                        g.AwayStats.PenaltyYards,
+                                        g.AwayStats.Turnovers,
+                                        g.AwayStats.Punts,
+                                        g.AwayStats.PuntYards,
+                                        g.AwayStats.TimeOfPosession)
+                                )).ToArray()
+                            )).ToArray()
+                        )).ToArray()
+                    )).ToArray());
+
+            return league;
+        }
+
+        private Dictionary<string, GameStats[]> PreLoadStats(XmlDocument data)
+        {
+            Dictionary<string, GameStats[]> gameStats = new Dictionary<string, GameStats[]>();
+            foreach (XmlElement stat in data["Bundle"]["Stats"].ChildNodes)
+            {
+                string team = stat["Team"].InnerText;
+                int week = int.Parse(stat["Week"].InnerText);
+                GameStats gameStat = new GameStats(
+                    int.Parse(stat["Touchdowns"].InnerText),
+                    int.Parse(stat["TouchdownsAllowed"].InnerText),
+                    int.Parse(stat["FieldGoals"].InnerText),
+                    int.Parse(stat["FirstDowns"].InnerText),
+                    int.Parse(stat["PassingYards"].InnerText),
+                    int.Parse(stat["RushingYards"].InnerText),
+                    int.Parse(stat["Penalties"].InnerText),
+                    int.Parse(stat["PenaltyYards"].InnerText),
+                    int.Parse(stat["Turnovers"].InnerText),
+                    int.Parse(stat["Punts"].InnerText),
+                    int.Parse(stat["PuntYards"].InnerText),
+                    stat["TimeOfPosession"].InnerText.ToTimeSpan()
+                );
+
+                if (!gameStats.ContainsKey(team))
+                {
+                    gameStats.Add(team, new GameStats[17]);
+                }
+                gameStats[team][week - 1] = gameStat;
+            }
+
+            return gameStats;
+        }
+
+        private List<Game> PreLoadGames(XmlDocument data, DateTime onDate)
+        {
+            Dictionary<string, GameStats[]> gameStats = PreLoadStats(data);
+
+            List<Game> games = new List<Game>();
+            foreach (XmlElement game in data["Bundle"]["Games"].ChildNodes)
+            {
+                int week = int.Parse(game["Week"].InnerText);
+                string homeTeam = game["HomeTeam"].InnerText;
+                string awayTeam = game["AwayTeam"].InnerText;
+                DateTime date = DateTime.Parse(game["Date"].InnerText);
+                if (date < onDate)
+                {
+                    games.Add(new Game(
+                        date,
+                        int.Parse(game["Week"].InnerText),
+                        homeTeam,
+                        awayTeam,
+                        int.Parse(game["HomePoints"].InnerText),
+                        int.Parse(game["AwayPoints"].InnerText),
+                        gameStats[homeTeam][week - 1],
+                        gameStats[awayTeam][week - 1]
+                    ));
+                }
+            }
+
+            return games;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FootballObjects;
 
 namespace ConsoleApp
 {
@@ -10,23 +8,73 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            string hello = "HELLO WORLD";
-            int i = 0;
+            LeagueBuilder builder = new LeagueBuilder();
 
-            while (i < hello.Length)
+            League NFL = builder.BuildNFLLeague("../../NFLData/NFLStats_2018.xml", "../../NFLData/NFL_2018.xml");
+
+            Team[] AllTeams = new Team[0];
+            foreach (Conference conf in NFL.Conferences)
             {
-                Console.WriteLine(hello.Substring(0, i));
-                i++;
-            }
-            while (i >= 0)
-            {
-                Console.WriteLine(hello.Substring(0, i));
-                i--;
+                foreach (Division div in conf.Divisions)
+                {
+                    foreach (Team team in div.Teams)
+                    {
+                        Array.Resize(ref AllTeams, AllTeams.Length + 1); // increase the array size by one
+                        AllTeams[AllTeams.Length - 1] = team; // add the team to the last slot of the array
+                    }
+                }
             }
 
-            Console.WriteLine("Shall we have another go? Press 'Q' to quit, any other key to continue");
-            if (Console.ReadKey().Key != ConsoleKey.Q)
-                Main(null);
+            // Sort the array of teams by name
+            Array.Sort(AllTeams, delegate (Team t1, Team t2) {
+                return t1.ToString().CompareTo(t2.ToString());
+            });
+            // Display the teams
+            PrintTeams(AllTeams);
+
+            // Sort the array of teams by win percentage
+            Array.Sort(AllTeams, delegate (Team t1, Team t2) {
+                return t2.WinPercentage.CompareTo(t1.WinPercentage); // t2 is first because we want descending order (highest win % first)
+            });
+            PrintTeams(AllTeams);
+
+            // Sort the array of teams by points scored
+            Array.Sort(AllTeams, delegate (Team t1, Team t2) {
+                return t2.PointsScored.CompareTo(t1.PointsScored); // t2 is first because we want descending order (highest points scored first)
+            });
+            PrintTeams(AllTeams);
+
+            // Compare New Orleans to Tampa Bay
+            string teamX = "NO";
+            string teamY = "TB";
+            int result = NFL.GetTeam(teamX).HeadToHead(teamY);
+            if (result == 1)
+                Console.WriteLine("{0} has a winning record against {1}", NFL.GetTeam(teamX), NFL.GetTeam(teamY));
+            else
+                Console.WriteLine("{0} does not have a winning record against {1}", NFL.GetTeam(teamX), NFL.GetTeam(teamY));
+
+            Pause();
+        }
+
+        static void PrintTeams(Team[] Teams)
+        {
+            foreach (Team t in Teams)
+            {
+                Console.WriteLine("{0} {1} have won {2} games, lost {3} games, scored {4} points and {5} touchdowns",
+                    t.City.PadRight(13),
+                    t.NickName.PadRight(10),
+                    t.Wins,
+                    t.Losses,
+                    t.PointsScored,
+                    t.TouchdownsScored);
+            }
+            Pause();
+        }
+
+        static void Pause()
+        {
+            Console.WriteLine("--Press Enter--");
+            Console.ReadLine();
         }
     }
 }
